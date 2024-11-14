5506280115180b1bf00ee30a8a6631a280ea51d6
howtam <35475547+howtam@users.noreply.github.com>
2024-11-14 12:50:10 -0500

    More info added
    describe how to use it.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # data_extraction_from_text_files
 Use regular expressions and flowcharts concept to profile text files for accurate data extraction
+
+Description of python files:
+IO.py -- define the input of text and output of data.  Can be augmented as needed.
+extracter.py -- define how data is extracted from each type of line.  Can be augmented as needed.
+sample_profile.py -- one dict structure to describe all types of lines from input and how to handle them
+			one dict structure to describe the flow between line types
+workbench.py -- To run process.  Don't touch it.
+
+Additional information is in the scripts.
--- sample_profile.py
+++ sample_profile.py
@@ -1,5 +1,49 @@
 from extracter import *
 
+"""
+Two types of lines: one carries data and one doesn't.
+Most important element is 'rules', whose value is a list of regular expressions to describe the line.
+
+The one that does not carry data has only 'rules'.
+
+For the other type there is addictional dict:
+
+'usage': {  'id': 101,
+            'parser': TokenExtracter(['acctNum'], [1]),} }
+
+'id': every type of line has unique id integer number from 100 and on.
+'parser' carries a object that is configured to extract the data from this line type
+
+The digits from hundred and up designate a level.  For example, 101 mean it belongs to level 1.
+The highest level number in this structure is always for output.  In our case, trans_1 and trans_2.  
+Workbench object realizes the highest level and output data when needed.  Right now the rule for triggering out is that when, in this case,
+any of the 900 level ids is seen the second time, the output will be done before processing the new line.
+Why the concept of level?
+For example there is account, subaccount, transaction types and transaction detail.  It is high level.  The text is organized like this:
+
+account
+    subaccount
+        transaction type
+            transaction detail
+            transaction detail  <--- A
+            transaction detail  <--- B
+        transaction type
+            transaction detail  <--- C
+    subaccount
+        transaction type
+            transaction detail  <--- D
+account  <--- E
+        transaction type
+            transaction detail
+
+There could be and is often the case that account information involves more than one line of text, as shown in the example below.  
+That's why the id numbers for account must be grouped together by ways of level.
+
+......        
+
+So there are four levels.
+
+"""
 regExConfig = {
     '__blank__': { 'rules': [r'^\s*$'], },
     '__dash__': { 'rules': [r'^-+\s*$'], },
@@ -16,6 +60,29 @@ regExConfig = {
               'usage': {'id': 902, 
                         'parser': RegExExtracter(['stock_2', 'bye_sell_2',  'amount_2'], r'TRANS2: ([A-Za-z]+)\s+(BUY|SELL)\s+(\d+)\s*$'),} },
     }
+
+"""
+for the sample structure about the flowchart is like this:
+flowChart = {
+    '__start__': ['account'],
+    'account': ['subaccount', 'transaction type'],   # notice E
+    'subaccount': ['transaction type'],
+    'transaction type': ['transaction detail'],
+    'transaction detail' ; ['transaction detail', 'transaction type', 'subaccount', '__end__'],    # notice A, B and C  
+                                                                                                #'__end__' is where a structure is completed
+                                                                                                
+It must have the '__start__' key to indicate what types of first relevant line should be read
+'__end__' shows the completion of the structure and also where the input should end otherwise there could be some data problem.
+
+"""
+
+"""
+In this flowchart, there are two types of transaction information.  One is described with trans_1 and one with ( trans_1, trans_2 )
+From trans_1 if the next stop is acct_1, which means it is a complete transaction description.  Of course the next stop can be trans_1 
+but that would be a new one.  if the next one is trans_2, there is more data for this transaction.
+
+Both trans_1 and trans_2 can lead to __end__
+"""
 
 flowChart = {
     '__start__': ['acct_1'],
