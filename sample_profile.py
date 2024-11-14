ea98e83f6c72521006923fd3526b509c050eb146
howtam <35475547+howtam@users.noreply.github.com>
2024-11-14 11:47:33 -0500

    Clean profile setup
    Clean profile setup

--- sample_profile.py
+++ sample_profile.py
@@ -1,41 +1,20 @@
-import re
-
-class ExtracterBase:
-    def __init__(self, fieldNames):
-        self.fieldNames = fieldNames
-        self.numFields = len(self.fieldNames)
-        
-    def run(self, line):
-        return self.fieldNames, []
-        
-    def getFieldNames(self):
-        return self.fieldNames
-        
-class RegExExtracter(ExtracterBase):
-    def __init__(self, regex, fieldNames):
-        super().__init__(fieldNames)
-        self.regex = regex
-        self.re = re.compile(self.regex)
-        
-    def run(self, line):
-        m = self.re.findall(line)
-        return self.fieldNames, [m[0]] if self.numFields == 1 else list(m[0])
+from extracter import *
 
 regExConfig = {
     '__blank__': { 'rules': [r'^\s*$'], },
     '__dash__': { 'rules': [r'^-+\s*$'], },
     'acct_1': { 'rules': [r'^ACCT: [\d]+'], 
                'usage': {'id': 101, 
-                        'parser': RegExExtracter(r'ACCT: (\d+)', ['acctNum']),} },
+                        'parser': TokenExtracter(['acctNum'], [1]),} },
     'acct_2': { 'rules': [r'^NAME: [^\s]+'], 
                'usage': {'id': 102, 
-                        'parser': RegExExtracter(r'NAME: ([A-Za-z]+)\s*$', ['name']),} },
+                        'parser': RegExExtracter(['name'], r'NAME: ([A-Za-z]+)\s*$'),} },
     'trans_1': { 'rules': [r'^TRANS1: '], 
               'usage': {'id': 901, 
-                        'parser': RegExExtracter( r'TRANS1: ([A-Za-z]+)\s+(BUY|SELL)\s+(\d+)\s*$', ['stock_1', 'bye_sell_1',  'amount_1']),} },
+                        'parser': TokenExtracter(['stock_1', 'bye_sell_1',  'amount_1'], [1,2,3]),} },
     'trans_2': { 'rules': [r'^TRANS2: '], 
               'usage': {'id': 902, 
-                        'parser': RegExExtracter( r'TRANS2: ([A-Za-z]+)\s+(BUY|SELL)\s+(\d+)\s*$', ['stock_2', 'bye_sell_2',  'amount_2']),} },
+                        'parser': RegExExtracter(['stock_2', 'bye_sell_2',  'amount_2'], r'TRANS2: ([A-Za-z]+)\s+(BUY|SELL)\s+(\d+)\s*$'),} },
     }
 
 flowChart = {
